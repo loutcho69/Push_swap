21c8c8b1155de6f948975b0792b1b716db7c5965
Benjamin Theveny <btheveny@z1r9p4.42lyon.fr>
2026-02-24 18:17:52 +0100

    norminette disorder except count_mistakes has one argument too many

--- disorder.c
+++ disorder.c
@@ -6,75 +6,78 @@
 /*   By: btheveny <btheveny@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/24 16:24:38 by btheveny          #+#    #+#             */
-/*   Updated: 2026/02/24 17:53:49 by btheveny         ###   ########.fr       */
+/*   Updated: 2026/02/24 18:17:20 by btheveny         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "stack.h"
 
-static int  get_nth_content(t_list *lst, size_t index)
+static int	get_nth_content(t_list *lst, size_t index)
 {
-    size_t k = 0;
-    while (lst && k < index)
-    {
-        lst = lst->next;
-        k++;
-    }
-    if (!lst)
-        return (0);
-    return (lst->content);
+	size_t	k;
+
+	k = 0;
+	while (lst && k < index)
+	{
+		lst = lst->next;
+		k++;
+	}
+	if (!lst)
+		return (0);
+	return (lst->content);
 }
 
-size_t  stack_len(t_list *lst) // safely handle empty list
+size_t	stack_len(t_list *lst)
 {
-    size_t i;
+	size_t	i;
 
-    i = 0;
-    while (lst)
-    {
-        i++;
-        lst = lst->next;
-    }
-    return (i);
+	i = 0;
+	while (lst)
+	{
+		i++;
+		lst = lst->next;
+	}
+	return (i);
 }
 
-void    count_mistakes_and_pairs(size_t i, int *mistakes, int *total_pairs, size_t len, t_list *a)
+void	count_mistakes_and_pairs(size_t i, int *mistakes,
+	int *total_pairs, size_t len, t_list *a)
 {
-    size_t j;
-    
-    while (i < len - 1)
-    {
-    j = i + 1;
-        while (j < len)
-        {
-            (*total_pairs)++;
-            if (get_nth_content(a, i) > get_nth_content(a, j))
-                (*mistakes)++;
-            j++;
-        }
-    i++;
-    }
+	size_t	j;
+
+	while (i < len - 1)
+	{
+		j = i + 1;
+		while (j < len)
+		{
+			(*total_pairs)++;
+			if (get_nth_content(a, i) > get_nth_content(a, j))
+				(*mistakes)++;
+			j++;
+		}
+		i++;
+	}
 }
 
-float   disorder(t_list *a)
+float	disorder(t_list *a)
 {
-    int     mistakes;
-    int     total_pairs;
-    size_t  i;
-    size_t  len;
-    float   result;
+	int		mistakes;
+	int		total_pairs;
+	size_t	i;
+	size_t	len;
+	float	result;
 
-    if (!a)
-        return (0.0f);
-    i = 0;
-    total_pairs = 0;
-    mistakes = 0;
-    len = stack_len(a);
-    if (len < 2)
-        return (0.0f);
-    printf("total_pairs = %d and mistakes = %d\n", total_pairs, mistakes);
-    count_mistakes_and_pairs(i, &mistakes, &total_pairs, len, a);
-    printf("total_pairs = %d and mistakes = %d\n", total_pairs, mistakes);
-    result = (float)mistakes / (float)total_pairs;
-    return (result);
+	if (!a)
+		return (0.0f);
+	i = 0;
+	total_pairs = 0;
+	mistakes = 0;
+	len = stack_len(a);
+	if (len < 2)
+		return (0.0f);
+	printf("total_pairs = %d and mistakes = %d\n", total_pairs, mistakes);
+	count_mistakes_and_pairs(i, &mistakes, &total_pairs, len, a);
+	printf("total_pairs = %d and mistakes = %d\n", total_pairs, mistakes);
+	result = (float)mistakes / (float)total_pairs;
+	return (result);
 }
