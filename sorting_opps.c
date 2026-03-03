1644e2eb77bac1b5eccbc2bf3a58779cf9ec0d1e
Lou Broue <loutcho69@MBP-de-Lou.lan>
2026-03-03 11:02:00 +0100

    add write for sorting opps

--- sorting_opps.c
+++ sorting_opps.c
@@ -6,7 +6,7 @@
 /*   By: lobroue <lobroue@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/24 00:55:01 by lobroue           #+#    #+#             */
-/*   Updated: 2026/03/01 02:36:12 by lobroue          ###   ########.fr       */
+/*   Updated: 2026/03/03 11:01:19 by lobroue          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ void    swap_a(t_list **stack_a)
     tmp = (*stack_a)->next->index;
     (*stack_a)->next->index = (*stack_a)->index;
     (*stack_a)->index = tmp;
+    write(1,"sa",2);
 }
 void    swap_b(t_list **stack_b)
 {
@@ -37,6 +38,7 @@ void    swap_b(t_list **stack_b)
     tmp = (*stack_b)->next->index;
     (*stack_b)->next->index =(*stack_b)->index;
     (*stack_b)->index = tmp;
+    write(1,"sb",2);
 }
 
 void    swap_ab(t_list **stack_a, t_list **stack_b)
@@ -44,6 +46,7 @@ void    swap_ab(t_list **stack_a, t_list **stack_b)
     // ss : sa and sb at the same time.
     swap_a(stack_a);
     swap_b(stack_b);
+    write(1,"ss",2);
 }
 
 void    push_a(t_list   **stack_a, t_list **stack_b)
@@ -65,11 +68,12 @@ void    push_a(t_list   **stack_a, t_list **stack_b)
         *stack_b = node->next;
     }
     ft_lstadd_front(stack_a, node);
+    write(1,"pa",2);
 }
 
 void    push_b(t_list   **stack_b, t_list **stack_a)
 {
-    // pa : push b - take the first element at the top of a and put it at the top of b. 
+    // pb : push b - take the first element at the top of a and put it at the top of b. 
     // Do nothing if a is empty.
     t_list *node;
     
@@ -86,38 +90,45 @@ void    push_b(t_list   **stack_b, t_list **stack_a)
         *stack_a = node->next;
     }
     ft_lstadd_front(stack_b, node);
+    write(1,"pb",2);
 }
 void    rotate_a(t_list **stack_a)
 {
     // ra : rotate a - shift up all elements of stack a by 1.
     //  The first element becomes the last one.
     *stack_a = (*stack_a)->next;
+    write(1,"ra",2);
 }
 
 void    rotate_b(t_list **stack_b)
 {
     //rb : rotate b - shift up all elements of stack b by 1. The first element becomes the last one.
     *stack_b = (*stack_b)->next;
+    write(1,"rb",2);
 }
 void    rotate_ab(t_list **stack_a, t_list **stack_b)
 {
     // rr : ra and rb at the same time.
     rotate_a(stack_a);
     rotate_b(stack_b);
+    write(1,"rr",2);
 }
 void    rev_rotate_a(t_list **stack_a)
 {
     // rra : reverse rotate a - shift down all elements of stack a by 1. The last element becomes the first one.
     *stack_a = (*stack_a)->prev;
+    write(1,"rra",2);
 }
 void    rev_rotate_b(t_list **stack_b)
 {
     // rrb : reverse rotate b - shift down all elements of stack b by 1. The last element becomes the first one.
     *stack_b = (*stack_b)->prev;
+    write(1,"rrb",2);
 }
 void    rev_rotate_ab(t_list **stack_a, t_list **stack_b)
 {
    // rrr : rra and rrb at the same time.
     rev_rotate_a(stack_a);
     rev_rotate_b(stack_b);
+    write(1,"rrr",2);
 }
