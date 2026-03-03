cfb2e49719ac6933ee3a546b2555ca901c3df91e
Benjamin Theveny <btheveny@z2r8p3.42lyon.fr>
2026-03-03 14:44:15 +0100

    just tested the makefile

--- build/utils/parser_utils.d
+++
@@ -1,3 +0,0 @@
-build/utils/parser_utils.o: utils/parser_utils.c utils/../push_swap.h
-
-utils/../push_swap.h:
--- build/utils/parsing_index.d
+++
@@ -1,3 +0,0 @@
-build/utils/parsing_index.o: utils/parsing_index.c utils/../push_swap.h
-
-utils/../push_swap.h:
