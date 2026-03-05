4baf8593f8264551dbb182f39359790aba4815d7
Benjamin Theveny <btheveny@z2r8p4.42lyon.fr>
2026-03-05 14:28:04 +0100

    normed medium sort

--- algorithms/medium_sort.c
+++ algorithms/medium_sort.c
@@ -6,7 +6,7 @@
 /*   By: btheveny <btheveny@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/05 14:24:13 by btheveny          #+#    #+#             */
-/*   Updated: 2026/03/05 14:24:35 by btheveny         ###   ########.fr       */
+/*   Updated: 2026/03/05 14:27:24 by btheveny         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,13 +25,15 @@ static void	medium_sort(t_list **stack_a, t_list **stack_b, t_data *data)
 		i = data->chunk_size;
 		while (i > 0 && (*stack_a))
 		{
-			if ((*stack_a)->index >= data->chunk_start && (*stack_a)->index < data->chunk_end)
+			if ((*stack_a)->index >= data->chunk_start
+				&& (*stack_a)->index < data->chunk_end)
 			{
 				push_b(stack_b, stack_a);
 				i--;
 			}
 			else
-				rotate_opti(stack_a, 'a',data->chunk_start,data->chunk_end);
+				rotate_opti(stack_a, 'a',
+					data->chunk_start, data->chunk_end);
 		}
 		data->chunk_start = data->chunk_end;
 		data->chunk_end = data->chunk_end + data->chunk_size;
