dd0d51cb37cb26960256bb2a81351aa88b80ca68
Benjamin Theveny <btheveny@z2r8p4.42lyon.fr>
2026-03-05 18:32:59 +0100

    added checker function TODO

---
+++ checker.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checker.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: btheveny <btheveny@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/05 18:29:17 by btheveny          #+#    #+#             */
+/*   Updated: 2026/03/05 18:32:40 by btheveny         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+/*TODO : le checker doit lire les instructions donnes par notre push swap,
+il doit essayer de trier la stack et de verifier si apres les operations
+la stack est effectivement triee
+si oui OK
+Else KO
+Sur le principe on a pas grande chose de plus a faire faut juste
+etre capable de lire sur stdout et ensuite de faire les operations sur notre stack
+et une fois que c;est lu on fait les operations sur notre stack et on regarde les index
+si les index sont dans l'ordre alors on est bons*/
+
+
