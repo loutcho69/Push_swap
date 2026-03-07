35aa6563fd7e97fdf0a104e015adcdca562f8c4d
Benjamin Theveny <btheveny@z1r7p4.42lyon.fr>
2026-03-07 18:14:47 +0100

    added structure for parser bonus

--- bonus/checker.c
+++ bonus/checker.c
@@ -6,33 +6,77 @@
 /*   By: btheveny <btheveny@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/05 18:29:17 by btheveny          #+#    #+#             */
-/*   Updated: 2026/03/05 18:54:08 by btheveny         ###   ########.fr       */
+/*   Updated: 2026/03/07 18:14:09 by btheveny         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "push_swap.h"
+#include "push_swap_bonus.h"
+#include "../push_swap.h"
+#include <stdio.h>
 
-/*TODO : le checker doit lire les instructions donnes par notre push swap,
-il doit essayer de trier la stack et de verifier si apres les operations
-la stack est effectivement triee
-si oui OK
-Else KO
-Sur le principe on a pas grande chose de plus a faire faut juste
-etre capable de lire sur stdout et ensuite de faire les operations sur notre stack
-et une fois que c;est lu on fait les operations sur notre stack et on regarde les index
-si les index sont dans l'ordre alors on est bons*/
+static int	parse_opps(char *opps, t_list **stack_a, t_list **stack_b)
+{
+	if (!(ft_strcmp(opps, "sa\n")))
+		sa(stack_a);
+	else if (!(ft_strcmp(opps, "sb\n")))
+		sb(stack_b);
+	else if (!(ft_strcmp(opps, "ss\n")))
+		ss(stack_a, stack_b);
+	else if (!(ft_strcmp(opps, "pa\n")))
+		pa(stack_a, stack_b);
+	else if (!(ft_strcmp(opps, "pb\n")))
+		pb(stack_a, stack_b);
+	else if (!(ft_strcmp(opps, "ra\n")))
+		ra(stack_a);
+	else if (!(ft_strcmp(opps, "rb\n")))
+		rb(stack_b);
+	else if (!(ft_strcmp(opps, "rr\n")))
+		rr(stack_a, stack_b);
+	else if (!(ft_strcmp(opps, "rra\n")))
+		rra(stack_a);
+	else if (!(ft_strcmp(opps, "rrb\n")))
+		rrb(stack_b);
+	else if (!(ft_strcmp(opps, "rrr\n")))
+		rrr(stack_a, stack_b);
+	else if (opps)
+		return (0);
+	return (1);
+}
 
+static int build_stack_from_array(t_list **stack, int *arr, size_t n)
+{
+	size_t  i;
+	t_list  *node;
 
-int	read_stdout(t_list **stack_a, t_list **stack_b) //je vais renvoyer des int pour savoir si ca marche ou pas
+	*stack = NULL;
+	i = 0;
+	while (i < n)
+	{
+		node = node_new(arr[i]);
+		if (!node)
+		{
+			stack_clear(stack);
+			return (0); /* échec allocation */
+		}
+		/* pour ce checker simple on assigne l'index séquentiellement */
+		node->index = (int)i;
+		ft_node_add_back(stack, node);
+		i++;
+	}
+	return (1);
+}
+
+static int	read_stdout(t_list **stack_a, t_list **stack_b) //je vais renvoyer des int pour savoir si ca marche ou pas
 {
 	char	*opps;
 
 	opps = get_next_line(0);
+	printf("%s\n", opps);
 	while (opps)
 	{
 		if (!parse_opps(opps, stack_a, stack_b))
 		{
-			ft_printf("Error\n", 2); //tjrs printf en stderr
+			printf("Error\n"); //tjrs printf en stderr
 			free(opps);
 			stack_clear(stack_a);
 			stack_clear(stack_b);
@@ -40,9 +84,47 @@ int	read_stdout(t_list **stack_a, t_list **stack_b) //je vais renvoyer des int p
 		}
 		free(opps);
 		opps = get_next_line(0);
+		printf("%s\n", opps);
 	}
+	return (1);
 }
 
 
+int main(void)
+{
+	t_list	*stack_a;
+	t_list	*stack_b;
+	t_list	*cur;
+	int		arr[] = {1, 2, 3};
+	int		ok;
 
+	stack_a = NULL;
+	stack_b = NULL;
+	cur = stack_a;
+
+	if (!build_stack_from_array(&stack_a, arr, 3))
+		return (1);
+	if (!read_stdout(&stack_a, &stack_b))
+		return (1);
+
+	cur = stack_a;
+	ok = 1;
+	while (cur && cur->next)
+	{
+		if (cur->value > cur->next->value)
+		{
+			ok = 0;
+			break ;
+		}
+		cur = cur->next;
+	}
+	if (ok)
+		printf("OK\n");
+	else
+		printf("KO\n");
+
+	stack_clear(&stack_a);
+	stack_clear(&stack_b);
+	return (0);
+}
 
--- bonus/push_swap_bonus.h
+++ bonus/push_swap_bonus.h
@@ -6,20 +6,22 @@
 /*   By: btheveny <btheveny@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/05 18:44:42 by btheveny          #+#    #+#             */
-/*   Updated: 2026/03/05 18:46:55 by btheveny         ###   ########.fr       */
+/*   Updated: 2026/03/07 18:01:42 by btheveny         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_BONUS_H
 # define PUSH_SWAP_BONUS_H
-
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 3
+#endif
 # include <stdlib.h>
 # include <unistd.h>
 
 /*GNL and its utils*/
 
 char	*get_next_line(int fd);
-size_t	ft_strlen(char *s);
+//size_t	ft_strlen(char *s);
 void	ft_memmove(char *dst, char *src, size_t n);
 char	*ft_append(char *line, char *buf, size_t n);
 void	ft_shift(char *basin, ssize_t *len, ssize_t cut);
