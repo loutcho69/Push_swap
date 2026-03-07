e851099081d8d1970ec11026fa6f3ebddae3c1f1
Benjamin Theveny <btheveny@z1r7p4.42lyon.fr>
2026-03-07 18:42:20 +0100

    added temporary opps to check if checker works

---
+++ bonus/opps/opps.c
@@ -0,0 +1,146 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   opps.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: btheveny <btheveny@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/07 18:37:48 by btheveny          #+#    #+#             */
+/*   Updated: 2026/03/07 18:39:49 by btheveny         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//tmp opps.c to test
+#include "../push_swap_bonus.h"
+
+void	push_a(t_list **stack_a, t_list **stack_b, t_data *data)
+{
+	t_list	*node;
+
+	if (!stack_b || !(*stack_b))
+		return ;
+	node = (*stack_b);
+	if (node->next == node)
+		*stack_b = NULL;
+	else
+	{
+		node->next->prev = node->prev;
+		node->prev->next = node->next;
+		*stack_b = node->next;
+	}
+	ft_lstadd_front(stack_a, node);
+	write(1, "pa\n", 3);
+	data->pa_count += 1;
+	data->opps_count += 1;
+}
+
+void	push_b(t_list **stack_b, t_list **stack_a, t_data *data)
+{
+	t_list	*node;
+
+	if (!stack_a || !(*stack_a))
+		return ;
+	node = (*stack_a);
+	if (node->next == node)
+		*stack_a = NULL;
+	else
+	{
+		node->next->prev = node->prev;
+		node->prev->next = node->next;
+		*stack_a = node->next;
+	}
+	ft_lstadd_front(stack_b, node);
+	write(1, "pb\n", 3);
+	data->pb_count += 1;
+	data->opps_count += 1;
+}
+void	rev_rotate_a(t_list **stack_a, t_data *data)
+{
+	*stack_a = (*stack_a)->prev;
+	write(1, "rra\n", 4);
+	data->rra_count += 1;
+	data->opps_count += 1;
+}
+void	rev_rotate_b(t_list **stack_b, t_data *data)
+{
+	*stack_b = (*stack_b)->prev;
+	write(1, "rrb\n", 4);
+	data->rrb_count += 1;
+	data->opps_count += 1;
+}
+void	rev_rotate_ab(t_list **stack_a, t_list **stack_b, t_data *data)
+{
+	t_data	tmp;
+
+	tmp = *data;
+	rev_rotate_a(stack_a, &tmp);
+	rev_rotate_b(stack_b, &tmp);
+	write(1, "rrr\n", 4);
+	data->rrr_count += 1;
+	data->opps_count += 1;
+}
+void	rotate_a(t_list **stack_a, t_data *data)
+{
+	*stack_a = (*stack_a)->next;
+	write(1, "ra\n", 3);
+	data->ra_count += 1;
+	data->opps_count += 1;
+}
+
+void	rotate_b(t_list **stack_b, t_data *data)
+{
+	*stack_b = (*stack_b)->next;
+	write(1, "rb\n", 3);
+	data->rb_count += 1;
+	data->opps_count += 1;
+}
+void	rotate_ab(t_list **stack_a, t_list **stack_b, t_data *data)
+{
+	t_data	tmp;
+
+	tmp = *data;
+	rotate_a(stack_a, &tmp);
+	rotate_b(stack_b, &tmp);
+	write(1, "rr\n", 3);
+	data->rr_count += 1;
+	data->opps_count += 1;
+}
+// ici toutes les opperations compatbilisees
+void	swap_a(t_list **stack_a, t_data *data)
+{
+	int	tmp;
+
+	// if(!(*stack_a)->prev || !(*stack_a)->next)
+	//     return ;
+	tmp = (*stack_a)->next->index;
+	(*stack_a)->next->index = (*stack_a)->index;
+	(*stack_a)->index = tmp;
+	write(1, "sa\n", 3);
+	data->sa_count += 1;
+	data->opps_count += 1;
+}
+void	swap_b(t_list **stack_b, t_data *data)
+{
+	int	tmp;
+
+	// if(!(*stack_b)->prev || !(*stack_b)->next)
+	//     return ;
+	tmp = (*stack_b)->next->index;
+	(*stack_b)->next->index = (*stack_b)->index;
+	(*stack_b)->index = tmp;
+	write(1, "sb\n", 3);
+	data->sb_count += 1;
+	data->opps_count += 1;
+}
+
+void	swap_ab(t_list **stack_a, t_list **stack_b, t_data *data)
+{
+	t_data	tmp;
+
+	tmp = *data;
+	swap_a(stack_a, &tmp);
+	swap_b(stack_b, &tmp);
+	write(1, "ss\n", 3);
+	data->ss_count += 1;
+	data->opps_count += 1;
+}
--- bonus/push_swap_bonus.h
+++ bonus/push_swap_bonus.h
@@ -6,9 +6,11 @@
 /*   By: btheveny <btheveny@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/05 18:44:42 by btheveny          #+#    #+#             */
-/*   Updated: 2026/03/07 18:01:42 by btheveny         ###   ########.fr       */
+/*   Updated: 2026/03/07 18:41:45 by btheveny         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*tmporary opps*/
 
 #ifndef PUSH_SWAP_BONUS_H
 # define PUSH_SWAP_BONUS_H
@@ -17,6 +19,7 @@
 #endif
 # include <stdlib.h>
 # include <unistd.h>
+# include "../push_swap.h"
 
 /*GNL and its utils*/
 
@@ -26,5 +29,18 @@ void	ft_memmove(char *dst, char *src, size_t n);
 char	*ft_append(char *line, char *buf, size_t n);
 void	ft_shift(char *basin, ssize_t *len, ssize_t cut);
 char	*ft_take_nl(char *line, char *basin, ssize_t *len, ssize_t nl);
+
+/* temporary prototypes for ops in bonus/opps/opps.c */
+void	push_a(t_list **stack_a, t_list **stack_b, t_data *data);
+void	push_b(t_list **stack_b, t_list **stack_a, t_data *data);
+void	rev_rotate_a(t_list **stack_a, t_data *data);
+void	rev_rotate_b(t_list **stack_b, t_data *data);
+void	rev_rotate_ab(t_list **stack_a, t_list **stack_b, t_data *data);
+void	rotate_a(t_list **stack_a, t_data *data);
+void	rotate_b(t_list **stack_b, t_data *data);
+void	rotate_ab(t_list **stack_a, t_list **stack_b, t_data *data);
+void	swap_a(t_list **stack_a, t_data *data);
+void	swap_b(t_list **stack_b, t_data *data);
+void	swap_ab(t_list **stack_a, t_list **stack_b, t_data *data);
 
 #endif
