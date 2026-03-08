2f68c7ce3189153c87fde7a7bef14c1b51a6a48f
Lou Broue <loutcho69@MacBook-Pro-de-Lou.local>
2026-03-08 02:26:22 +0100

    clean

---
+++ build/algo/disorder.d
@@ -0,0 +1,2 @@
+build/algo/disorder.o: algo/disorder.c push_swap.h
+push_swap.h:
---
+++ main.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lobroue <lobroue@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/02 18:59:55 by btheveny          #+#    #+#             */
+/*   Updated: 2026/03/08 01:24:11 by lobroue          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	main(int argc, char **argv)
+{
+	t_list	*stack_a;
+	t_list	*stack_b;
+	t_data	data;
+	float	d;
+
+	stack_a = NULL;
+	stack_b = NULL;
+	init_values_data(&stack_a, &data);
+	parse_input(argc, argv, &stack_a, &data);
+	data.len_stack = count_node(stack_a);
+	if (!stack_a)
+		return (write(2,"Error\n",6));
+	d = disorder(stack_a);
+	data.disorder = d;
+	flag_dispatcher(&stack_a, &stack_b, &data);
+	if (data.bench)
+		print_bench(&data);
+	ft_lstclear(&stack_a, data.len_stack);
+	stack_b = NULL;
+	return (0);
+}
